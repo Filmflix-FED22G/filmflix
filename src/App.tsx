f86808609a23d1a9d94960bd13b1634064aebc9a
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-06 23:51:34 +0100

    added Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Hero from './components/Hero';
+import heroImage from '/img/hero-interstellar.png';
 
 function App() {
   return (
     <>
-      <h1>FILMFLIX!!</h1>
+      <Hero
+        title="Interstellar"
+        quote="We're not meant to save the world. We're meant to leave it."
+        imageUrl={heroImage}
+      />
       <Outlet />
     </>
   );
