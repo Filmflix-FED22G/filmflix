22373b69ded0f4f9eca779cb2cbe4dbceccef6c7
Martina Kreivi <martinakreivi@gmail.com>
2023-12-06 12:01:49 +0100

    Style compnents for FooterList and FooterListItem

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -4,20 +4,44 @@ import styled from 'styled-components';
 const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
-
   background-color: #000;
   color: #fff;
-  height: 200px;
-  padding: 50px;
+  height: 12rem;
+  padding: 3rem;
+`;
+const FooterList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const FooterListItem = styled.li`
+  padding: 0.5rem 0;
 `;
 
 const Footer: React.FC = () => {
   return (
     <FooterContainer>
       <div style={{ flex: 1 }}>Div 1</div>
-      <div style={{ flex: 1 }}>Div 2</div>
-      <div style={{ flex: 1 }}>Div 3</div>
-      <div style={{ flex: 1 }}>Div 4</div>
+      <div style={{ flex: 1 }}>
+        <h3>INFO</h3>
+        <FooterList>
+          <FooterListItem>Customer Service</FooterListItem>
+          <FooterListItem>Terms & Conditions</FooterListItem>
+          <FooterListItem>Privacy Policy</FooterListItem>
+          <FooterListItem>Cookie Settings</FooterListItem>
+        </FooterList>
+      </div>
+      <div style={{ flex: 1 }}>
+        <h3>ABOUT US</h3>
+        <FooterList>
+          <FooterListItem>Press</FooterListItem>
+          <FooterListItem>News</FooterListItem>
+          <FooterListItem>Career</FooterListItem>
+        </FooterList>
+      </div>
+      <div style={{ flex: 1 }}>
+        <h3>SUBSCRIBE</h3>
+      </div>
       <div style={{ flex: 1 }}>Div 5</div>
     </FooterContainer>
   );
