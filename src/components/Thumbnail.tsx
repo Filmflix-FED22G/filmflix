6e8851cff4f7e1735f1c9bee38535f2b93beb5c6
stenbumling <marcusrosin@outlook.com>
2023-12-06 16:03:52 +0100

    feat: add bookmark icon(s) to thumbnail

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import magnifyingGlass from '../../public/icons/bookmark-selected.svg';
+import bookmarkSelected from '../../public/icons/bookmark-selected.svg';
+import bookmarkUnselected from '../../public/icons/bookmark-unselected.svg';
 
 export default function Thumbnail({ movie }: { movie: any }) {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  function handleBookmarkClick() {
+    setIsBookmarked(!isBookmarked);
+  }
+
   return (
     <ThumbnailContainer>
       <MovieThumbnail src={movie.thumbnail} alt={movie.title + ' poster'} />
@@ -10,41 +18,45 @@ export default function Thumbnail({ movie }: { movie: any }) {
         <RatingBadge>
           <span>{movie.rating}</span>
         </RatingBadge>
-        <BookmarkIcon src={magnifyingGlass}></BookmarkIcon>
+        <BookmarkIcon
+          onClick={handleBookmarkClick}
+          src={isBookmarked ? bookmarkSelected : bookmarkUnselected}
+          alt="Bookmark button"
+        ></BookmarkIcon>
       </SecondaryInfoContainer>
-      <TitleContainer>
-        <Title>{movie.title}</Title>
-      </TitleContainer>
+      <h4>{movie.title}</h4>
     </ThumbnailContainer>
   );
 }
 
 const ThumbnailContainer = styled.div`
   width: 16rem;
-  margin: 1rem;
   display: flex;
-  margin: 0.4rem;
   flex-direction: column;
+  margin: 0 2rem 3rem 0;
 `;
 
 const MovieThumbnail = styled.img`
   object-fit: cover;
   margin-bottom: 0.5rem;
+  transition: 0.2s ease-in-out;
 
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    opacity: 0.6;
+  }
+
+  &:active {
+    transform: scale(0.98);
   }
 `;
 
 const SecondaryInfoContainer = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
-  width: 100%;
 
   p {
-    padding-top: 3px;
+    margin: 0.2rem 0.5rem 0 0;
   }
 `;
 
@@ -52,7 +64,6 @@ const RatingBadge = styled.div`
   height: 1rem;
   background-color: #434343;
   border-radius: 0.3rem;
-  margin-left: 0.5rem;
   padding: 0.2rem 0.3rem 0.1rem 0.3rem;
   display: flex;
   justify-content: center;
@@ -61,28 +72,21 @@ const RatingBadge = styled.div`
   span {
     font-size: 0.6rem;
     color: white;
-    line-height: 1;
   }
-`;
-
-const TitleContainer = styled.div`
-  padding: 0rem 0rem 0.2rem 0rem;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-`;
-
-const Title = styled.h3`
-  font-size: 1.2rem;
-  color: white;
 `;
 
 const BookmarkIcon = styled.img`
   width: 1.3rem;
   height: 1.3rem;
   margin: 0 0 0 auto;
+  transition: 0.1s ease-in-out;
+
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    opacity: 0.6;
+  }
+
+  &:active {
+    transform: scale(0.9);
   }
 `;
