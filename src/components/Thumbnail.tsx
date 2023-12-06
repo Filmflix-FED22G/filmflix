469a8522ecd1c591048f2562c7e65ce88af5053e
stenbumling <marcusrosin@outlook.com>
2023-12-06 13:49:19 +0100

    style: add basic styling to thumbnail

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -2,14 +2,50 @@ import styled from 'styled-components';
 
 export default function Thumbnail({ movie }: { movie: any }) {
   return (
-    <>
-      <MovieThumbnail src={movie.thumbnail} alt={movie.title} />
-      <div>{movie.title}</div>
-    </>
+    <ThumbnailContainer>
+      <MovieThumbnail src={movie.thumbnail} alt={movie.title + ' poster'} />
+      <TitleContainer>
+        <Title>{movie.title}</Title>
+        <BookmarkIcon></BookmarkIcon>
+      </TitleContainer>
+    </ThumbnailContainer>
   );
 }
 
+const ThumbnailContainer = styled.div`
+  width: 16rem;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
 const MovieThumbnail = styled.img`
-  width: 10rem;
   object-fit: cover;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
+`;
+
+const TitleContainer = styled.div`
+  padding: 1rem 0.5rem 1rem 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  background-color: #1c1c1c;
+`;
+
+const Title = styled.h3`
+  font-size: 1.2rem;
+  color: white;
+`;
+
+const BookmarkIcon = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  aspect-ratio: 1/1;
+  background-color: white;
+  border-radius: 50%;
+  margin-left: 2.5rem;
 `;
