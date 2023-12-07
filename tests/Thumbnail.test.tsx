ebaa49cf46d2646f5fae8a71be1b70ce929a5947
stenbumling <marcusrosin@outlook.com>
2023-12-07 14:06:34 +0100

    test: add test for rendering error placeholder

--- tests/Thumbnail.test.tsx
+++ tests/Thumbnail.test.tsx
@@ -1,13 +1,14 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import movies from '../data/movies.json';
 import Thumbnail from '../src/components/Thumbnail';
 import { Movie } from '../types/movieTypes';
+import userEvent from "@testing-library/user-event";
 
 describe('Thumbnail Component', () => {
   const mockMovie: Movie = movies[0];
 
-  it('renders everything correctly', async () => {
+  it('renders everything correctly', () => {
     render(
       <Router>
         <Thumbnail movie={mockMovie} />
@@ -15,13 +16,34 @@ describe('Thumbnail Component', () => {
     );
 
     const poster = screen.getByAltText('The Shawshank Redemption poster');
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
     fireEvent.load(poster)
-
     expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+    expect(poster).toBeInTheDocument();
+
     expect(screen.getByText('The Shawshank Redemption')).toBeInTheDocument();
     expect(screen.getByText('1994')).toBeInTheDocument();
     expect(screen.getByText('R')).toBeInTheDocument();
-    expect(poster).toBeInTheDocument();
-    expect(screen.getByAltText('Bookmark button')).toBeInTheDocument();
+    expect(screen.getByAltText('Bookmark icon')).toBeInTheDocument();
+  });
+
+  it('renders error placeholder when image fails to load', () => {
+    render(
+      <Router>
+        <Thumbnail movie={mockMovie} />
+      </Router>,
+    );
+
+    const poster = screen.getByAltText('The Shawshank Redemption poster');
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    fireEvent.error(poster)
+    expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+    expect(screen.getByText('Image not found')).toBeInTheDocument();
+    expect(poster.style.display).toBe('none');
+
+    expect(screen.getByText('The Shawshank Redemption')).toBeInTheDocument();
+    expect(screen.getByText('1994')).toBeInTheDocument();
+    expect(screen.getByText('R')).toBeInTheDocument();
+    expect(screen.getByAltText('Bookmark icon')).toBeInTheDocument();
   });
 });
