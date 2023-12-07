9a17a3e5951814e7f73eba4a236324cefe5db098
stenbumling <marcusrosin@outlook.com>
2023-12-07 11:07:14 +0100

    refactor: move thumbnail testing to bookmark page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,24 +1,9 @@
-import movies from '../../data/movies.json';
-import { Movie } from '../../types/movieTypes';
-import Thumbnail from '../components/Thumbnail';
-import styled from 'styled-components';
-
 function HomePage() {
   return (
     <div>
       <h2>HomePage</h2>
-      <MovieList>
-      {movies.map((movie: Movie) => (
-        <Thumbnail movie={movie} />
-      ))}
-      </MovieList>
     </div>
   );
 }
 
 export default HomePage;
-
-const MovieList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
