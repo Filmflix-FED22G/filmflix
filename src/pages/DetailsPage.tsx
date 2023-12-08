7bf5289674f3ef909c06bbb76a528cf0be1279d4
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-08 13:47:27 +0100

    using params to find movies when clicking on thumbnail link

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,7 +1,20 @@
+import { useParams } from 'react-router-dom';
+import movies from '../../data/movies.json';
+import slugify from '../utils/slugify';
+
 function DetailsPage() {
+  const { title } = useParams();
+  const movie = movies.find((m) => slugify(m.title) === title);
+
+  if (!movie) {
+    return <div>Movie not found</div>;
+  }
+
   return (
     <div>
-      <h1>DetailsPage</h1>
+      <h1>{movie.title}</h1>
+      <p>{movie.synopsis}</p>
+      <img src={movie.thumbnail} alt={movie.title} />
     </div>
   );
 }
