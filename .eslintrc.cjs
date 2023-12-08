ec8f2d3aff792790a703d698911fbbea6cc671cb
stenbumling <marcusrosin@outlook.com>
2023-12-08 13:35:07 +0100

    lint: add some eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-console': 'warn',
+    eqeqeq: ['error', 'always'],
+    curly: 'error',
+    'no-var': 'error',
   },
-}
+};
