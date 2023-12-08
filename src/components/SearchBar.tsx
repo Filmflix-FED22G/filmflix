782a723f01ed7932080f57a272fcdc368ca67e3f
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-12-08 13:38:17 +0100

    formatting in dropdown

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,67 +22,6 @@ interface SearchBarProps {
   $showInMobile?: boolean;
 }
 
-const SearchBarContainer = styled.div<SearchBarContainerProps>`
-  position: relative;
-  display: flex;
-  border: 0.3rem solid #fff;
-
-  input {
-    flex-grow: 1;
-    border: none;
-    padding: 0.5rem;
-    outline: none;
-    color: black;
-    background-color: white;
-  }
-
-  button {
-    flex-shrink: 0;
-    flex-basis: 2rem;
-    width: 2rem;
-    height: 2rem;
-    border: none;
-    background-color: black;
-    color: white;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    outline: none;
-
-    &:hover {
-      background-color: #aa8c21;
-    }
-  }
-
-  @media (max-width: 768px) {
-    display: ${({ $showInMobile }) => ($showInMobile ? 'flex' : 'none')};
-    width: 90%;
-    margin: auto;
-  }
-`;
-
-const SearchResultsDropdown = styled.div`
-  position: absolute;
-  width: auto;
-  max-height: 500px;
-  overflow-y: auto;
-  background-color: var(--color-dark-grey);
-  z-index: 10;
-`;
-
-const ThumbnailContainer = styled.div`
-  width: 100%;
-  display: block;
-  margin: 0.5rem 0;
-`;
-
-const StyledSVG = styled.img`
-  width: 1rem;
-  height: 1rem;
-`;
-
 function SearchBar({ $showInMobile = false }: SearchBarProps) {
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
@@ -101,7 +40,7 @@ const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   if (query.length > 0) {
     const filtered = movies
       .filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()))
-      .slice(0, 5); // Limit the number of suggestions
+      .slice(0, 5);
 
     setFilteredMovies(filtered);
     setShowDropdown(true);
@@ -134,7 +73,7 @@ const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   }, []);
 
   return (
-    <div ref={searchBarRef}>
+    <SearchContainer ref={searchBarRef} $showInMobile={$showInMobile}>
       <form onSubmit={handleFormSubmit}>
         <SearchBarContainer $showInMobile={$showInMobile}>
           <input type="text" placeholder="Search for a movie" value={searchQuery} onChange={handleInputChange} />
@@ -147,14 +86,90 @@ const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       {showDropdown && filteredMovies.length > 0 && (
         <SearchResultsDropdown>
           {filteredMovies.map(movie => (
-            <ThumbnailContainer>
-            <Thumbnail key={movie.title} movie={movie} />
+            <ThumbnailContainer key={movie.title}>
+              <Thumbnail movie={movie} />
             </ThumbnailContainer>
           ))}
         </SearchResultsDropdown>
       )}
-    </div>
+    </SearchContainer>
   );
 }
 
 export default SearchBar;
+
+const SearchContainer = styled.div<SearchBarProps>`
+  position: relative;
+  width: 100%;
+  max-width: 20rem;
+  display: block;
+
+  @media (max-width: 768px) { 
+    width: 100%;
+    margin: 1rem auto;
+    max-width: 40rem;
+    display: ${({ $showInMobile }) => ($showInMobile ? 'block' : 'none')};
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    width: 100%;
+    margin: 1rem auto;
+    display: block; 
+    max-width: 40rem;
+  }
+`;
+
+const SearchBarContainer = styled.div<SearchBarContainerProps>`
+  display: flex;
+  border: 0.3rem solid #fff;
+  width: 100%;
+
+  input {
+    flex-grow: 1;
+    border: none;
+    padding: 0.5rem;
+    outline: none;
+    color: black;
+    background-color: white;
+  }
+
+  button {
+    flex-shrink: 0;
+    flex-basis: 2rem;
+    width: 2rem;
+    height: 2rem;
+    border: none;
+    background-color: black;
+    color: white;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    outline: none;
+
+    &:hover {
+      background-color: #aa8c21;
+    }
+  }
+`;
+
+const SearchResultsDropdown = styled.div`
+  position: absolute;
+  width: 100%;
+  max-height: 500px;
+  overflow-y: auto;
+  background-color: var(--color-dark);
+  z-index: 10;
+`;
+
+const ThumbnailContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledSVG = styled.img`
+  width: 1rem;
+  height: 1rem;
+`;
