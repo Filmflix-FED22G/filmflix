2ff196845d3128c106198e20d09e2f784822066c
Martina Kreivi <martinakreivi@gmail.com>
2023-12-11 08:58:36 +0100

    Better solution for the carousel component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,120 +1,51 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import data from '../../data/movies.json';
-import { Movie } from '../../types/movieTypes';
 
-const Carousel = () => {
-  const trendingMovies = data.filter((movie) => movie.isTrending === true);
-  console.log('trendingMovies: ', trendingMovies);
-  const [movies, setMovies] = useState<Movie[]>(trendingMovies);
-  const [currentIndex, setCurrentIndex] = useState(0);
+interface CarouselProps {
+  data: Movie[];
+}
 
-  useEffect(() => {
-    setMovies(trendingMovies);
-  }, [data]);
-
-  const handleNext = () => {
-    if (currentIndex > movies.length) {
-      return setCurrentIndex(0);
-    }
-    setCurrentIndex((prevIndex) => (prevIndex + 3) % movies.length);
-  };
-
-  const handlePrev = () => {
-    if (currentIndex === 0) return;
-    setCurrentIndex((prevIndex) => (prevIndex - 3) % movies.length);
-  };
-  const itemsPerPage = 3;
-
-  return (
-    <CarouselContainer>
-      <CarouselWrapper>
-        <CarouselHeading>TRENDING</CarouselHeading>
-        <CarouselItemsWrapper>
-          <NavButton onClick={handlePrev}>Previous</NavButton>
-          {movies
-            .slice(currentIndex, currentIndex + itemsPerPage)
-            .map((m, index: number) => (
-              <CarouselItem key={index}>
-                <img
-                  style={{
-                    zIndex: '10',
-                    height: '17rem',
-                    width: '100%',
-                  }}
-                  src={m.thumbnail}
-                  alt=""
-                  onError={(e) => {
-                    const target = e.target as HTMLImageElement;
-                    target.src =
-                      'https://static.vecteezy.com/system/resources/thumbnails/022/059/000/small/no-image-available-icon-vector.jpg';
-                  }}
-                />
-                <CarouselFilmTitle>{m.title}</CarouselFilmTitle>
-                <CarouselFilmTitle>{m.year}</CarouselFilmTitle>
-                <CarouselFilmTitle>{m.rating}</CarouselFilmTitle>
-              </CarouselItem>
-            ))}
-          <NavButton onClick={handleNext}>Next</NavButton>
-        </CarouselItemsWrapper>
-      </CarouselWrapper>
-    </CarouselContainer>
-  );
-};
-
-const CarouselContainer = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  background-color: var(--color-dark-grey);
-  overflow: hidden;
-`;
+interface Movie {
+  title: string;
+  year: number;
+  rating: string;
+  actors: string[];
+  genre: string;
+  synopsis: string;
+  thumbnail: string;
+  isTrending?: boolean;
+}
 
 const CarouselWrapper = styled.div`
+  background-color: var(--color-dark-grey);
+  width: 100%;
   display: flex;
-  align-items: center;
   padding: 2rem;
-  transition: transform 0.5s ease-in-out;
-  width: 100%;
-`;
-
-const CarouselItemsWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 3rem;
-  width: 100%;
-`;
-
-const CarouselItem = styled.div`
-  width: 10rem;
-  box-sizing: border-box;
 `;
 const CarouselHeading = styled.h3`
   margin-top: 0;
   margin-bottom: 1rem;
   font-family: 'Oswald', sans-serif;
 `;
-const CarouselFilmTitle = styled.p`
-  margin: 0;
-  font-size: 0.75rem;
+const CarouselImage = styled.img`
+  height: 15rem;
+  padding: 0.5rem;
 `;
 
-const NavButton = styled.button`
-  transform: translateY(-50%);
-  background: transparent;
-  border: none;
-  padding: 10px;
-  font-size: 16px;
-  cursor: pointer;
-
-  &:first-child {
-    left: 0;
-  }
-
-  &:last-child {
-    right: 0;
-  }
-`;
+const Carousel: React.FC<CarouselProps> = ({ data }) => {
+  return (
+    <CarouselWrapper>
+      {data.map((movie, index) => (
+        // Render each movie item in the carousel
+        <div key={index}>
+          <CarouselImage src={movie.thumbnail} alt={movie.title} />
+          <p>{movie.title}</p>
+          <p>{movie.year}</p>
+          <p>{movie.rating}</p>
+        </div>
+      ))}
+    </CarouselWrapper>
+  );
+};
 
 export default Carousel;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,9 +6,13 @@ import Carousel from '../components/Carousel';
 console.log('moviedata: ', data);
 
 const HomePage: React.FC = () => {
+  const trendingMovies = data.filter((movie) => movie.isTrending);
+  const recommendedMovies = data.filter((movie) => !movie.isTrending);
+
   return (
     <div>
-      <Carousel />
+      <Carousel data={trendingMovies} />
+      <Carousel data={recommendedMovies} />
       {/* Other content for your homepage */}
     </div>
   );
