570a2666f57588f0457b6f1f95777dbeb56c7648
Martina Kreivi <martinakreivi@gmail.com>
2023-12-11 14:24:43 +0100

    Heading on mobile screen

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -24,7 +24,10 @@ const Carousel: React.FC<CarouselProps> = ({ data, heading }) => {
       <CarouselHeading>
         {heading && <Heading>{heading}</Heading>}
       </CarouselHeading>
-      <MoviesWrapper data-testid="movies-wrapper">
+      <CarouselHeadingMobile>
+        {heading && <Heading>{heading}</Heading>}
+      </CarouselHeadingMobile>
+      <MoviesWrapper>
         {data.map((movie) => (
           <Thumbnail movie={movie} />
         ))}
@@ -36,10 +39,15 @@ const Carousel: React.FC<CarouselProps> = ({ data, heading }) => {
 const CarouselWrapper = styled.div`
   background-color: var(--color-dark-grey);
   width: 100%;
-  display: flex;
   padding: 1rem;
   margin: 2rem 0;
   display: flex;
+  flex-direction: column;
+
+  @media screen and (min-width: 760px) {
+    flex-direction: row;
+    justify-content: left;
+  }
 `;
 
 const CarouselHeading = styled.div`
@@ -51,6 +59,16 @@ const CarouselHeading = styled.div`
   }
 `;
 
+const CarouselHeadingMobile = styled.div`
+  display: none;
+
+  @media screen and (max-width: 760px) {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 const MoviesWrapper = styled.div`
   display: flex;
   overflow-x: scroll;
