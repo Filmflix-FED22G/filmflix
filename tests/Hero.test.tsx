6707c8c6181b45a40afa5b586c7fdbdab21d3e35
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-12 23:55:43 +0100

    added test for navigating to the movie details page on link click (for movie featured in hero)

--- tests/Hero.test.tsx
+++ tests/Hero.test.tsx
@@ -1,11 +1,18 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import {
+  MemoryRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
 import movies from '../data/movies.json';
 import Hero from '../src/components/Hero';
+import MovieView from '../src/components/MovieView';
 
 describe('Hero', () => {
-  const mockMovie = movies[30];
+  const mockMovie = movies[30]; // Using movie Interstellar as mockMovie since it's featured in the Hero
 
   test('should render the correct movie title', () => {
     render(
@@ -26,6 +33,31 @@ describe('Hero', () => {
     expect(
       screen.getByText(
         "We're not meant to save the world. We're meant to leave it.",
+      ),
+    ).toBeInTheDocument();
+  });
+
+  test('should navigate to the correct movie details page on link click', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Hero movie={mockMovie} />} />
+          <Route path="/details/:title" element={<MovieView />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const viewMoreLink = screen.getByRole('link', { name: /view more/i });
+    await user.click(viewMoreLink);
+
+    // Check if the component for movie details is rendered
+    expect(screen.getByText('Interstellar')).toBeInTheDocument();
+    expect(screen.getByText('2014')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
       ),
     ).toBeInTheDocument();
   });
