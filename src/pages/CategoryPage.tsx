49a57d2cba8cc37230d1dbd725463d6f8a41d493
Martina Kreivi <martinakreivi@gmail.com>
2023-12-12 10:24:32 +0100

    Added carousel for drama

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,13 +1,19 @@
-import CategoryDropdown from '../components/CategoryDropdown';
 import { Outlet } from 'react-router-dom';
+import { default as data } from '../../data/movies.json';
+import Carousel from '../components/Carousel';
+import CategoryDropdown from '../components/CategoryDropdown';
 
-function CategoryPage() {
+const CategoryPage: React.FC = () => {
+  const dramaMovies = data.filter((movie) => movie.genre === 'Drama');
+  const recommendedMovies = data.filter((movie) => !movie.isTrending);
+
   return (
     <div>
       <CategoryDropdown />
       <Outlet />
+      <Carousel data={dramaMovies} heading="DRAMA" />
     </div>
   );
-}
+};
 
 export default CategoryPage;
