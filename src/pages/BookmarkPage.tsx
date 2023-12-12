b2f5b84a868f82ea51daebd41c6dae92a40049c7
stenbumling <marcusrosin@outlook.com>
2023-12-12 14:27:00 +0100

    style: add grid to bookmark

--- src/pages/BookmarkPage.tsx
+++ src/pages/BookmarkPage.tsx
@@ -1,28 +1,48 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import movies from '../../data/movies.json';
 import { Movie } from '../../types/movieTypes';
 import Thumbnail from '../components/Thumbnail';
-import { useEffect } from 'react';
 
 function BookmarkPage() {
   useEffect(() => {
     document.title = 'Bookmarks';
   }, []);
   return (
-    <div>
-      <h1>Bookmarks</h1>
-      <MovieList>
+    <Container>
+      <Title>Bookmarks</Title>
+      <MovieGrid>
         {movies.map((movie: Movie) => (
           <Thumbnail key={movie.title} movie={movie} />
         ))}
-      </MovieList>
-    </div>
+      </MovieGrid>
+    </Container>
   );
 }
 
 export default BookmarkPage;
 
-const MovieList = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+const Container = styled.div`
+  margin: 3rem 2rem;
+  max-width: 120rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(12rem, 12rem));
+  grid-gap: 2rem;
+  justify-content: center;
+`;
+
+const Title = styled.h1`
+  grid-column: 1 / -1;
+  text-align: left;
+  padding: 2rem 0;
+  @media (max-width: 929px) {
+    text-align: center;
+  }
+`;
+
+const MovieGrid = styled.div`
+  grid-column: 1 / -1;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(12rem, 12rem));
+  grid-gap: 2rem;
 `;
