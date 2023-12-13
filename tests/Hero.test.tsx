904f4e69d49b188d0edffe0ecb66f9e4f296e512
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-12-13 20:13:56 +0100

    edited test for hero after small refactoring

--- tests/Hero.test.tsx
+++ tests/Hero.test.tsx
@@ -1,24 +1,72 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {
+  MemoryRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
 import Hero from '../src/components/Hero';
+import MovieView from '../src/components/MovieView';
+import { MovieProvider } from '../src/contexts/MovieContext';
+import { Movie } from '../types/movieTypes';
+import mockMovies from './mocks/mockMovies.json';
 
 describe('Hero', () => {
-  test('should render the correct movie title', () => {
-    const mockTitle = 'Interstellar';
-    render(<Hero title={mockTitle} heroImageUrl="" quote="" />);
+  const mockMovie: Movie = mockMovies[30]; // Using movie Interstellar as mockMovie since it's featured in the Hero
 
-    expect(screen.getByText(mockTitle)).toBeInTheDocument();
+  test('should render the correct movie title', () => {
+    render(
+      <MovieProvider>
+        <Router>
+          <Hero movie={mockMovie} />
+        </Router>
+      </MovieProvider>,
+    );
+    expect(screen.getByText(mockMovie.title)).toBeInTheDocument();
   });
 
-  test('should render the quote when provided', () => {
-    const mockTitle = 'Interstellar';
-    const mockImage = 'img/hero-interstellar.png';
-    const mockQuote =
-      "We're not meant to save the world. We're meant to leave it.";
+  test('should render the correct movie quote', () => {
     render(
-      <Hero title={mockTitle} heroImageUrl={mockImage} quote={mockQuote} />,
+      <MovieProvider>
+        <Router>
+          <Hero movie={mockMovie} />
+        </Router>
+      </MovieProvider>,
     );
 
-    expect(screen.getByText(mockQuote)).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        "We're not meant to save the world. We're meant to leave it.",
+      ),
+    ).toBeInTheDocument();
+  });
+
+  test('should navigate to the correct movie details page on link click', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MovieProvider>
+        <MemoryRouter initialEntries={['/']}>
+          <Routes>
+            <Route path="/" element={<Hero movie={mockMovie} />} />
+            <Route path="/details/:title" element={<MovieView />} />
+          </Routes>
+        </MemoryRouter>
+      </MovieProvider>,
+    );
+
+    const viewMoreLink = screen.getByRole('link', { name: /view more/i });
+    await user.click(viewMoreLink);
+
+    // Check if the component for movie details is rendered
+    expect(screen.getByText('Interstellar')).toBeInTheDocument();
+    expect(screen.getByText('2014')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
+      ),
+    ).toBeInTheDocument();
   });
 });
